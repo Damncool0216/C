f242641a45ecb21fff54b42c1a373e93308754c4
seven <Damncool0216@outlook.com>
2020-12-15 23:06:52 +0800

    master

--- text/1.3.c
+++ text/1.3.c
@@ -16,9 +16,33 @@ void pr_triangle(void)
 }
 
 //!用*打印菱形
+void pr_rhombus(void)
+{
+    int n;
+    printf("输入打印菱形的行数（奇数）：");
+    scanf("%d",&n);
+    int i,j,k=(n+1)/2;
+    for(i=1;i<=k;i++)
+    {
+        for(j=k-i;j>=0;j--)
+        printf(" ");
+        for(j=2*i-1;j>0;j--)
+        printf("*");
+        printf("\n");
+    }
+      for(i=k-1;i>=1;i--)
+    {
+        for(j=k-i;j>=0;j--)
+        printf(" ");
+        for(j=2*i-1;j>0;j--)
+        printf("*");
+        printf("\n");
+    }
 
+}
 int main()
 {
-    pr_triangle();
+    //pr_triangle();
+    pr_rhombus();
     return 0;
 }
